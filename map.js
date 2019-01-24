b8e5e56777d88fd4aa99e9884611e5b9173b2a9b
samajo <37985947+samajo@users.noreply.github.com>
2019-01-24 00:44:53 -0500

    Update map.js

--- map.js
+++ map.js
@@ -186,7 +186,7 @@ style: 'mapbox://styles/saj2mw/cjnc49sx20bih2rmu768iwzaa'});
         for (i=0; i<layers.length; i++) {
           // get the session variable for each candy type (the candy count)
           // the format of each element in the array will be {candyType: "candy-bear", candyCount: 3}
-          data.push({tagType: layers[i][1], tagCount: + localStorage.getItem(layers[i][0].properties.UP_VOTES)});
+          data.push({tagType: layers[i][1], tagCount: + localStorage.getItem(layers[i][0]).properties.UP_VOTES});
           console.log(layers[i][0]);
         }
 
